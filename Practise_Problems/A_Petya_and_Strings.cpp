2072da718c2ba9574bf1ebd10e092646a6bccf77
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2022-03-31 10:29:46 +0530

    more code

--- Practise_Problems/A_Petya_and_Strings.cpp
+++ Practise_Problems/A_Petya_and_Strings.cpp
@@ -6,18 +6,48 @@ using namespace std;
 
 int main()
 {
-    int res = 0;
-    string r, s;
-    cin >> r >> s;
+    int i;
+    string s, r;
+    cin >> s >> r;
 
-    for (int i = 0; i < r.length(); i++)
+    for (int i = 0; i < s.length(); i++)
     {
-        if (r[i] - s[i] == 32)
-            res = 0;
-        else if ((r[i] - s[i]) < 0 && r[i] - s[i] != 32)
-            res = -1;
-        else if ((r[i] - s[i]) > 0 && r[i] - s[i] != 32)
-            res = 1;
+        s[i] = tolower(s[i]);
+        r[i] = tolower(r[i]);
     }
-    cout << res << endl;
+    if (s == r)
+    {
+        cout << "0" << endl;
+    }
+    else
+    {
+        for (int i = 0; i < s.length(); i++)
+        {
+            if ((s[i] - r[i]) < 0)
+            {
+                cout << "-1";
+                break;
+            }
+            else if ((s[i] - r[i]) > 0)
+            {
+                cout << "1";
+                break;
+            }
+        }
+    }
+
+    // int res = 0;
+    // string r, s;
+    // cin >> r >> s;
+
+    // for (int i = 0; i < r.length(); i++)
+    // {
+    //     if (r[i] - s[i] == 32)
+    //         res = 0;
+    //     else if ((r[i] - s[i]) < 0 && r[i] - s[i] != 32)
+    //         res = -1;
+    //     else if ((r[i] - s[i]) > 0 && r[i] - s[i] != 32)
+    //         res = 1;
+    // }
+    // cout << res << endl;
 }
