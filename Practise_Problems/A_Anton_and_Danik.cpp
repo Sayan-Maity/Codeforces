9ce07b8adff9ece62a5b54232f6d2aa4cb4721cc
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2022-04-04 20:52:35 +0530

    more code

---
+++ Practise_Problems/A_Anton_and_Danik.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    int cntD = 0, cntA = 0, i;
+    int n;
+    string s;
+    cin >> n >> s;
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == 'D')
+            cntD++;
+        else if (s[i] == 'A')
+            cntA++;
+    }
+    (cntA == cntD) ? cout << "Friendship" : ((s[i] == 'A') && (cntA != cntD)) ? cout << "Anton"
+                                                                              : cout << "Danik";
+
+    return 0;
+}
