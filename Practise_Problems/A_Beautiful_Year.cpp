932f1abc41589135c9e1d23bd05494ff52947663
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2022-04-07 17:44:26 +0530

    more code

--- Practise_Problems/A_Beautiful_Year.cpp
+++ Practise_Problems/A_Beautiful_Year.cpp
@@ -1,3 +1,4 @@
+// <<<<<<<<<<<< Method-1 >>>>>>>>>>>>>
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -23,5 +24,30 @@ int main()
     }
     cout << y << endl;
 
+    return 0;
+}
+
+// <<<<<<<<<<<< Method-2 >>>>>>>>>>>>>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+
+    int y;
+    string s;
+    cin >> y;
+    while (true)
+    {
+        y++;
+        s = to_string(y);
+        if (s[0] != s[1] && s[1] != s[2] && s[2] != s[3] && s[1] != s[3] && s[0] != s[2] && s[2] != s[3])
+        {
+            cout << s << endl;
+            break;
+        }
+    }
     return 0;
 }
