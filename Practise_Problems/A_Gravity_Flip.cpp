9984f9a08e5b551636b587fe214cb49cb003c2a8
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2022-04-11 08:58:11 +0530

    more code

---
+++ Practise_Problems/A_Gravity_Flip.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a, a + n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+
+    return 0;
+}
