99d0add201959132f8b78af17f7e71e120e47dd7
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2022-06-23 23:30:39 +0530

    more code

---
+++ Practise_Problems/A_Plus_One_on_the_Subset.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+const int N = 3e5 + 5;
+// upper = 65-90 || lower = 97-122 || (lower-upper) = 32
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, maxi = INT_MIN, mini = INT_MAX;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] > maxi)
+                maxi = a[i];
+            if (a[i] < mini)
+                mini = a[i];
+        }
+        cout << maxi - mini << "\n";
+    }
+}
