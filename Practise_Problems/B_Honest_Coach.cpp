c09ef4fae610368f9d9bb6f5d80f668ba059c288
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2022-06-23 23:59:36 +0530

    more code

---
+++ Practise_Problems/B_Honest_Coach.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t, n, a[100];
+    cin >> t;
+    while (t--)
+    {
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        sort(a, a + n);
+        vector<int> v;
+        int dif = 0;
+        for (int i = 1; i < n; i++)
+        {
+            dif = a[i] - a[i - 1];
+            v.push_back(dif);
+        }
+
+        sort(v.begin(), v.end());
+        cout << v[0] << endl;
+    }
+    return 0;
+}
