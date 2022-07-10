677013606b771bee5bbc47d775f16e1cd7c157d8
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2022-07-10 21:10:13 +0530

    more code

--- Contest_Problems/A_Round_Down_The_Price.cpp
+++ Contest_Problems/A_Round_Down_The_Price.cpp
@@ -11,16 +11,15 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    ll n;
-    cin >> n;
-    if (n == 0)
-        cout << 1;
-    else if (n % 4 == 1)
-        cout << 8;
-    else if (n % 4 == 2)
-        cout << 4;
-    else if (n % 4 == 3)
-        cout << 2;
-    else
-        cout << 6;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int tmp = 1;
+        while (tmp * 10 <= n)
+            tmp *= 10;
+        cout << n - tmp << "\n";
+    }
 }
