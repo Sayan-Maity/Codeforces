551609d5a938043cc808c0def3bdc0336dcb3bcd
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2022-11-15 20:59:31 +0530

    more code

---
+++ Contest_Problems/A_The_Ultimate_Square.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+const int N=3e5+5;
+// upper = 65-90 || lower = 97-122 || (lower-upper) = 32
+
+
+int main() {
+	ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    ll t;
+    cin>>t;
+    while(t--){
+        int i;
+        cin>>i;
+        cout<< (i+1)/2<<"\n";
+    }
+}
---
+++ Contest_Problems/A_Working_Week.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define all(v) v.begin(),v.end()
+const int N=3e5+5;
+// upper = 65-90 || lower = 97-122 || (lower-upper) = 32
+
+
+int main() {
+	ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    ll t;
+    cin>>t;
+    while(t--){
+        
+    }
+}
